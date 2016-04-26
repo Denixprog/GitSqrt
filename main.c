b4755f720839f1c97f36dbd727feae56c8386194
Хрущев Денис Владимирович <ri57s17@awp05.csc.local>
2016-04-26 11:00:43 +0600

    fixed tabs

--- main.c
+++ main.c
@@ -6,47 +6,43 @@
 
 int solve(double a, double b, double c, double *x0, double *x1)
 {
-    double d;
-    d = (b * b) - (4 * a * c);
-    if ((d < 0) || ((a == 0) && (b == 0) && (c == 0)))
-    {
-        return 0;
-    }
-    else if (d == 0)
-    {
-        *x0 = -b / (2 * a);
-        return 1;
-    }
-
-    *x0 = (-b + sqrt(d)) / (2 * a);
-    *x1 = (-b - sqrt(d)) / (2 * a);
-    return 2;
-
+	double d;
+	d = (b * b) - (4 * a * c);
+	if ((d < 0) || ((a == 0) && (b == 0) && (c == 0)))
+	{
+		return 0;
+	}
+	else if (d == 0)
+	{
+		*x0 = -b / (2 * a);
+		return 1;
+	}
+	*x0 = (-b + sqrt(d)) / (2 * a);
+	*x1 = (-b - sqrt(d)) / (2 * a);
+	return 2;
 }
 
 int main()
 {
-    double a, b, c, x0, x1;
-    int roots;
-    printf("Input a, b, c: \n");
-    scanf("%lf %lf %lf", &a, &b, &c);
-    roots = solve(a, b, c, &x0, &x1);
-    if (roots == No_roots)
-    {
-        printf("No real roots\n");
-    }
-    else if (roots == One_root)
-    {
-        printf("Result:\n");
-        printf("x0 = %.2lf\n", x0);
-    }
-    else if (roots == Two_roots)
-    {
-        printf("Result:\n");
-        printf("x0 = %.2lf\n", x0);
-        printf("x1 = %.2lf\n", x1);
-    }
-
+	double a, b, c, x0, x1;
+	int roots;
+	printf("Input a, b, c: \n");
+	scanf("%lf %lf %lf", &a, &b, &c);
+	roots = solve(a, b, c, &x0, &x1);
+	if (roots == No_roots)
+	{
+		printf("No real roots\n");
+	}
+	else if (roots == One_root)
+	{
+		printf("Result:\n");
+		printf("x0 = %.2lf\n", x0);
+	}
+	else if (roots == Two_roots)
+	{
+		printf("Result:\n");
+		printf("x0 = %.2lf\n", x0);
+		printf("x1 = %.2lf\n", x1);
+	}
 	return 0;
-
 }
