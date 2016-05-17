70f8cd5faa1c74aaa8e9045461a04c65af7c4173
Хрущев Денис Владимирович <ri57s17@awp06.csc.local>
2016-05-17 10:08:13 +0600

    Fixed roots in solve function, added Fail_input

--- main.c
+++ main.c
@@ -3,23 +3,29 @@
 #define No_roots 0
 #define One_root 1
 #define Two_roots 2
+#define Fail_input -1
 
 int solve(double a, double b, double c, double *x0, double *x1)
 {
 	double d;
 	d = (b * b) - (4 * a * c);
-	if ((d < 0) || ((a == 0) && (b == 0) && (c == 0)))
+	//if ((a == 0) || (b == 0) || (c == 0))
+	if (a == 0)
 	{
-		return 0;
+		return Fail_input;
 	}
+	if (d < 0)
+	{
+		return No_roots;
+	}	
 	else if (d == 0)
 	{
 		*x0 = -b / (2 * a);
-		return 1;
+		return One_root;
 	}
 	*x0 = (-b + sqrt(d)) / (2 * a);
 	*x1 = (-b - sqrt(d)) / (2 * a);
-	return 2;
+	return Two_roots;
 }
 
 int main()
@@ -44,5 +50,9 @@ int main()
 		printf("x0 = %.2lf\n", x0);
 		printf("x1 = %.2lf\n", x1);
 	}
+	else if (roots == Fail_input)
+	{
+		printf("Incorrect input");
+	}	
 	return 0;
 }
